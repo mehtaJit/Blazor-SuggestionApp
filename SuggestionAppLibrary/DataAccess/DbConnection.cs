40d5129a3f538f91a980bb86f0fb282fb22df81a
mehtaJit <jmehta6549@gmail.com>
2022-05-02 15:56:22 +0100

    DB Connection Related Changes

--- SuggestionAppLibrary/DataAccess/DbConnection.cs
+++ SuggestionAppLibrary/DataAccess/DbConnection.cs
@@ -1,11 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
 
 namespace SuggestionAppLibrary.DataAccess;
 
 public class DbConnection
 {
+    private readonly IConfiguration _config;
+    private readonly IMongoDatabase _db;
+    private string _connectionId = "MongoDB";
+
+    public string DbName { get; private set; }
+    public string CategoryCollectionName { get; private set; } = "categories";
+    public string StatusCollectionName { get; private set; } = "statuses";
+    public string UserCollectionName { get; private set; } = "users";
+    public string SuggestionCollectionName { get; private set; } = "suggestions";
+    public MongoClient Client { get; private set; }
+    public IMongoCollection<CategoryModel> CategoryCollection { get; private set; }
+    public IMongoCollection<StatusModel> StatusCollection { get; private set; }
+    public IMongoCollection<UserModel> UserCollection { get; private set; }
+    public IMongoCollection<SuggestionModel> SuggestionCollection { get; private set; }
+
+    public DbConnection(IConfiguration config)
+    {
+        _config = config;
+
+        //Setup Client
+        Client = new MongoClient(_config.GetConnectionString(_connectionId));
+        DbName = _config["DatabaseName"];
+        _db = Client.GetDatabase(DbName);
+
+
+        SetupCollection();
+    }
+
+    private void SetupCollection()
+    {
+        CategoryCollection = _db.GetCollection<CategoryModel>(CategoryCollectionName);
+        StatusCollection = _db.GetCollection<StatusModel>(StatusCollectionName);
+        UserCollection = _db.GetCollection<UserModel>(UserCollectionName);
+        SuggestionCollection = _db.GetCollection<SuggestionModel>(SuggestionCollectionName);
+    }
 }
